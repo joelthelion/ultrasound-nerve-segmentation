59f9a2fce01bcd5219b788844de61efdd77eb743
Joël Schaerer <joel.schaerer@bioclinica.com>
2017-07-20 13:43:14 +0200

    batch norm experiment

--- view.sh
+++ view.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-c3d $1*.png -binarize -type uchar -o seg.png
+c3d $1*.png -thresh -inf 128 0 1 -type uchar -o seg.png
 itksnap -g datadir/$1*segmentation*.png -s seg.png
 #itksnap -g datadir/$1_4CH_ED.png -s seg.png
